a94adb6fa71e2224e11da0d467410f5f5648dd95
ruoyu <ruoyu_official@petalmail.com>
2023-05-10 20:51:11 +0800

    fix req prolem|2

--- src/policy/main.js
+++ src/policy/main.js
@@ -1,30 +1,45 @@
 function main() {
-  getConfig();
-  var year = new Date().getFullYear(),
-    AUTHOR = '若愚[RUOYU]';
-  ftext.innerHTML = `<b>&copy;${year}  &#64;${AUTHOR} ·</b>   <i>Design & Code</i>`;
+  fetch('./src/config')
+    .then(function (response) {
+      if (response.ok) {
+        return response.text();
+      }
+      throw new Error('Network response was not ok.');
+    })
+    .then(function (text) {
+      // 处理jsonData
+      console.log('config ok\n' + text);
+      var year = new Date().getFullYear(),
+        AUTHOR = '若愚[RUOYU]';
+      ftext.innerHTML = `<b>&copy;${year}  &#64;${AUTHOR} ·</b>   <i>Design & Code</i>`;
 
-  //首次初始化
-  Init(HEADER.scrollWidth, HEADER.scrollHeight);
-  if (DEBUG && deb_cmd) console.log('size:', CTX.width, CTX.height);
+      //首次初始化
+      Init(HEADER.scrollWidth, HEADER.scrollHeight);
+      if (DEBUG && deb_cmd) console.log('size:', CTX.width, CTX.height);
 
-  //入栈若干粒子
-  for (let i = 0; i < count; i++)
-    BALLs.push(
-      new Ball(
-        i,
-        '!me',
-        randType('size'),
-        randType('x'),
-        randType('y'),
-        randType('angle'),
-        randType('speed'),
-        randType('indigo')
-      )
-    );
-  // console.log(BALLs);
-  //回调动画
-  window.requestAnimationFrame((timestamp) => ANIMATION(timestamp, 0));
+      //入栈若干粒子
+      for (let i = 0; i < count; i++)
+        BALLs.push(
+          new Ball(
+            i,
+            '!me',
+            randType('size'),
+            randType('x'),
+            randType('y'),
+            randType('angle'),
+            randType('speed'),
+            randType('indigo')
+          )
+        );
+      // console.log(BALLs);
+      //回调动画
+      window.requestAnimationFrame((timestamp) => ANIMATION(timestamp, 0));
+    })
+    .catch(function (error) {
+      console.log(
+        'There has been a problem with your fetch operation: ' + error.message
+      );
+    });
 }
 //运行
 main();
